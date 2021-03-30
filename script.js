888bc4e7795ff228b1045bf8fb4dc87114faf970
zax5021 <zacharyacampbell1@gmail.com>
2021-03-29 20:28:41 -0700

    Finished with everything I think, local storage and rendering past items working as expected

--- script.js
+++ script.js
@@ -5,23 +5,25 @@ var pastSearchesEl = $("#pastSearches");
 var heroListEl = $(".hero-list-group");
 var heroCardEl = $(".hero-card");
 var forecastCardsEl = $("#forecastCards");
-// var forecastListUl = $(".forecast-list-group");
+var heroH5 = heroCardEl.children("h5")
 var pastSearchesUl = $("#pastSearchesUl")
+var iconEl = document.createElement("img")
 var searchCity;
 var searchCityCoords;
 var cityWeather;
 var saveCityObject= {};
 var savedSearches = [] ;
 var storedSearches;
+var savedSearcheslist;
+
 
 function searchSubmit (){
     searchCity = searchCityEl.val();
-    
+    pastSearchesUl.children().removeClass("active")
  getCoord();
 }
 function getCoord (){
-   
-    //made api key not work by adding F to beginning
+    
     var apiUrl = "https://api.opencagedata.com/geocode/v1/json?q=" + searchCity + "&key=3eb93ae01e55489188eadd7629028315&pretty=1";
     console.log(apiUrl)
     fetch(apiUrl)
@@ -50,15 +52,10 @@ function getCoord (){
     .catch(function (error) {
       console.error(error);
     });
-    // Commenting out to try the async thing
-    // getWeather();
 } 
 
 function getWeather() {
-    // searchCityCoords = {
-    //     latitude: 32.7174202,
-    //     longitude: -117.1627728
-    // }
+
     var weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + searchCityCoords.latitude + "&lon=" + searchCityCoords.longitude +"&units=imperial&exclude=hourly,minutely&appid=09067483d1a4888ae997aa4f31004a36";
     console.log(weatherApiUrl)
     fetch(weatherApiUrl)
@@ -74,56 +71,66 @@ function getWeather() {
    saveSearch();
     renderWeather()
      addSavedSearch();
-
     })
 }
 
 function saveSearch() {
+    if (!searchCityEl.val()) {
+        return
+    }
+    savedSearcheslist = savedSearches.map(({name})=> name)
+    console.log(savedSearcheslist)
+    if (savedSearcheslist.includes(searchCity)){
+        return
+    }
     saveCityObject = {
         name: searchCity,
         Coords: searchCityCoords,
     }
-    // if (savedSearches.length===0){
-    //     savedSearches= [
-    //         saveCityObject,
-    //     ]
-    // } else{
     console.log(savedSearches)   
-    // if(!savedSearches.includes({name:saveCityObject.name})){
     savedSearches.push(saveCityObject)
-    // }
-    // }
     localStorage.setItem("savedSearchesLocal", JSON.stringify(savedSearches))
-    console
 }
-// TODO write renderWeather function
 function renderWeather() {
     var curTemp = cityWeather.current.temp
     var curWindSpeed = cityWeather.current.wind_speed
     var curHumidity = cityWeather.current.humidity
     var curUV = cityWeather.current.uvi
+    var curIcon = cityWeather.current.weather[0].icon
     var curVars = [curTemp, curHumidity, curWindSpeed, curUV]
+    var heroH5 = heroCardEl.children("h5")
 
     heroCardEl.removeClass("hidden")
     heroCardEl.children("h5").text(searchCity)
+    $(iconEl).attr("src", "http://openweathermap.org/img/wn/" + curIcon + "2x.png")
+    $(iconEl).attr("class", "weatherIcon")
+    heroH5[0].appendChild(iconEl)
+    heroCardEl.children("h5").children().attr("src", "http://openweathermap.org/img/wn/" + curIcon + "@2x.png")
     for (var i=0; i<4; i++) {
         heroCardEl.children("ul").children("li").eq(i).children().first().text(curVars[i])
     }
+    if (curUV >= 3){
+        heroCardEl.children("ul").children("li").eq(3).children().first().attr("style", "color:orange")
+    } else if (curUV >=6){
+        heroCardEl.children("ul").children("li").eq(3).children().first().attr("style", "color:red")
+    } else {
+        heroCardEl.children("ul").children("li").eq(3).children().first().attr("style", "color:green")
+    }
 
     var dailyWeather = cityWeather.daily
+    var dailyIcon = 
 
-    
     forecastCardsEl.removeClass("hidden")
     for (i=1;i<6;i++){
         idx= i-1;
         forecastCardsEl.children().eq(idx).children("h5").text(moment().add(i, "d").format("l"))
+        forecastCardsEl.children().eq(idx).children("ul").children("li").eq(0).children().attr("src", "http://openweathermap.org/img/wn/" + cityWeather.daily[i].weather[0].icon + "@2x.png")
         forecastCardsEl.children().eq(idx).children("ul").children("li").eq(1).children().first().text(dailyWeather[i].temp.max)
         forecastCardsEl.children().eq(idx).children("ul").children("li").eq(2).children().first().text(dailyWeather[i].humidity)
         console.log(dailyWeather[i].temp.max)
     }
     console.log(searchCity)
 }
-
 
 function renderSavedSearches() {
     if(savedSearches.length===0){
@@ -132,6 +139,7 @@ function renderSavedSearches() {
     for (i=0; i<savedSearches.length; i++){
         var pastSearchesIl = document.createElement("li");
         $(pastSearchesIl).addClass("list-group-item list-group-item-action");
+        $(pastSearchesIl).attr("data-city", savedSearches[i].name);
         $(pastSearchesIl).text(savedSearches[i].name);
         pastSearchesUl[0].appendChild(pastSearchesIl);
     }    
@@ -139,12 +147,25 @@ function renderSavedSearches() {
 }}
 //TODO Clicking on lis should search that city and render the weather, also switch the li to active. Prevent new searches that match city name from adding a new li. 
 function addSavedSearch() {
+    if (!searchCityEl.val()) {
+        return
+    }
+    console.log(savedSearcheslist)
+    if (savedSearcheslist.includes(searchCity)){
+            var matchedCityIdx = savedSearcheslist.indexOf(searchCity)
+            console.log(matchedCityIdx)
+            pastSearchesUl.children().eq(matchedCityIdx).addClass("active")
+            return
+        }
     savedSearches = JSON.parse(localStorage.getItem("savedSearchesLocal"));
-   var pastSearchesIl = document.createElement("li");
+    var pastSearchesIl = document.createElement("li");
     $(pastSearchesIl).addClass("list-group-item list-group-item-action active");
     $(pastSearchesIl).text(savedSearches[savedSearches.length-1].name);
     pastSearchesUl[0].appendChild(pastSearchesIl);
+    searchCityEl.val("")
 }
+
+
 
 function init(){
   console.log(savedSearches)
@@ -155,8 +176,6 @@ function init(){
 for (i=0; i< storedSearches.length; i++){
     savedSearches.push(storedSearches[i])
 }
-    // savedSearches.push(storedSearches)
-    // }
     console.log(JSON.parse(localStorage.getItem("savedSearchesLocal")))
     console.log(savedSearches.length)
     console.log(typeof savedSearches)
@@ -165,3 +184,12 @@ for (i=0; i< storedSearches.length; i++){
 
 init()
 searchBtn.on("click", searchSubmit)
+pastSearchesUl.on("click", (event) => {
+    event.preventDefault();
+    searchCityEl.val("")
+    searchCity = $(event.target).text()
+    console.log(pastSearchesUl.children())
+    pastSearchesUl.children().removeClass("active")
+    $(event.target).addClass("active")
+    getCoord();
+})
